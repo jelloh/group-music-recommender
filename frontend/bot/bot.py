d35fc8246f4a8065f1ac1baaac8acf14f4b41524
Aileen <jelly8d@hotmail.com>
2021-04-04 23:46:28 -0400

    music bot updates! can play, queue, and search

--- frontend/bot/bot.py
+++ frontend/bot/bot.py
@@ -6,7 +6,17 @@ from discord.ext import commands
 class MusicBot(commands.Bot):
     def __init__(self):
         self._cogs = [p.stem for p in Path(".").glob("./bot/cogs/*.py")]
-        super().__init__(command_prefix=self.prefix, case_insensitive=True)
+        #print(self._cogs)
+
+        # Another option but we will just keep the "discord.Intents.all()" below instead 
+        # intents = discord.Intents.default()
+        # intents.members = True
+
+        super().__init__(
+            command_prefix=self.prefix, 
+            case_insensitive=True, 
+            intents=discord.Intents.all()
+            )
 
     def setup(self):
         print("Running setup...")
@@ -44,12 +54,18 @@ class MusicBot(commands.Bot):
     async def on_disconnect(self):
         print("Bot disconnected.")
 
+    # async def on_error(self, err, *args, **kwargs):
+    #     raise 
+
+    # async def on_command_error(self, ctx, exc):
+    #     raise getattr(exc, "original", exc)
+
     async def on_ready(self):
         self.client_id = (await self.application_info()).id
         print("Bot ready.")
 
     async def prefix(self, bot, msg):
-        return commands.when_mentioned_or("+")(bot, msg)
+        return commands.when_mentioned_or("~")(bot, msg)
 
     async def process_commands(self, msg):
         ctx = await self.get_context(msg, cls=commands.Context)
---
+++ frontend/bot/cogs/music.py
@@ -0,0 +1,254 @@
+import asyncio
+import datetime as dt
+import re
+import typing as t
+
+import discord
+import wavelink
+from discord.ext import commands
+
+URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+OPTIONS = {
+    "1️⃣": 0,
+    "2⃣": 1,
+    "3⃣": 2,
+    "4⃣": 3,
+    "5⃣": 4,
+}
+
+
+class AlreadyConnectedToChannel(commands.CommandError):
+    pass
+
+class NoVoiceChannel(commands.CommandError):
+    pass
+
+class QueueIsEmpty(commands.CommandError):
+    pass
+
+class NoTracksFound(commands.CommandError):
+    pass
+
+
+
+class Queue:
+    def __init__(self):
+        self._queue = []
+        self.position = 0
+
+    @property
+    def is_empty(self):
+        return not self._queue
+
+    def add(self, *args):
+        self._queue.extend(args)
+
+    @property
+    def first_track(self):
+        if not self._queue:
+            raise QueueIsEmpty
+
+        return self._queue[0]
+
+    def get_next_track(self):
+        if not self._queue:
+            raise QueueIsEmpty
+
+        self.position += 1
+
+        if self.position > len(self._queue) - 1:
+            return None
+
+        return self._queue[self.position]
+
+
+class Player(wavelink.Player):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queue = Queue()
+
+    async def connect(self, ctx, channel=None):
+        if self.is_connected:
+            raise AlreadyConnectedToChannel
+
+        if (channel := getattr(ctx.author.voice, "channel", channel)) is None:
+            raise NoVoiceChannel
+
+        await super().connect(channel.id)
+        return channel
+
+    async def teardown(self):
+        try:
+            await self.destroy()
+        except KeyError:
+            pass
+
+    async def add_tracks(self, ctx, tracks):
+        if not tracks:
+            raise NoTracksFound
+
+        if isinstance(tracks, wavelink.TrackPlaylist):
+            self.queue.add(*tracks.tracks)
+        elif len(tracks) == 1:
+            self.queue.add(tracks[0])
+            await ctx.send(f"Added {tracks[0].title} to the queue.")
+        else:
+            if (track := await self.choose_track(ctx, tracks)) is not None:
+                self.queue.add(track)
+                await ctx.send(f"Added {track.title} to the queue.")
+        
+        if not self.is_playing and not self.queue.is_empty:
+            await self.start_playback()
+
+    async def choose_track(self, ctx, tracks):
+        def _check(r, u):
+            return (
+                r.emoji in OPTIONS.keys()
+                and u == ctx.author
+                and r.message.id == msg.id
+            )
+
+        # TODO Would eventually be nice to have this show Hours:Minutes:Seconds 
+        # instead of just Minutes:Seconds (inside the description) 
+        embed = discord.Embed(
+            title="Choose a song",
+            description=(
+                "\n".join(
+                    f"**{i+1}.** {t.title} ({t.length//60000}:{str(t.length%60).zfill(2)})"
+                    for i, t in enumerate(tracks[:5])
+                )
+            ),
+            colour=ctx.author.colour,
+            timestamp=dt.datetime.utcnow()
+        )
+        embed.set_author(name="Query Results")
+        embed.set_footer(text=f"Invoked by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+
+        msg = await ctx.send(embed=embed)
+        for emoji in list(OPTIONS.keys())[:min(len(tracks), len(OPTIONS))]:
+            await msg.add_reaction(emoji)
+
+        try:
+            reaction, _ = await self.bot.wait_for("reaction_add", timeout=60.0, check=_check)
+        except asyncio.TimeoutError:
+            await msg.delete()
+            await ctx.message.delete()
+        else:
+            await msg.delete()
+            return tracks[OPTIONS[reaction.emoji]]
+
+    async def start_playback(self):
+        await self.play(self.queue.first_track)
+
+    async def advance(self):
+        try:
+            if (track := self.queue.get_next_track()) is not None:
+                await self.play(track)
+        except QueueIsEmpty:
+            pass
+        
+    
+class Music(commands.Cog, wavelink.WavelinkMixin):
+    def __init__(self, bot):
+        self.bot = bot
+        self.wavelink = wavelink.Client(bot=bot)
+        self.bot.loop.create_task(self.start_nodes())
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        """
+        Use this to determine when to leave the voice channel.
+        """
+        if not member.bot and after.channel is None: # check to see if member left the channel
+            if not [m for m in before.channel.members if not m.bot]: # check to see if member is bot
+                await self.get_player(member.guild).teardown() # disconnect the bot from the channel (ep4)
+
+    @wavelink.WavelinkMixin.listener()
+    async def on_node_ready(self, node):
+        print(f" Wavelink node `{node.identifier}` ready.")
+
+    @wavelink.WavelinkMixin.listener("on_track_stuck")
+    @wavelink.WavelinkMixin.listener("on_track_end")
+    @wavelink.WavelinkMixin.listener("on_track_exception")
+    async def on_player_stop(self, node, payload):
+        await payload.player.advance()
+
+
+
+    async def cog_check(self, ctx):
+        """
+        Automatically applied to all commands. Disallow commands sent from DM.
+        """
+        if isinstance(ctx.channel, discord.DMChannel):
+            await ctx.send("Music commands are not available in DMs.")
+            return False
+
+        return True
+
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
+
+        nodes = {
+            "MAIN": {
+                "host": "127.0.0.1",
+                "port": 2333,
+                "rest_uri": "http://127.0.0.1:2333",
+                "password": "youshallnotpass",
+                "identifier": "MAIN",
+                "region": "us_central", 
+            }
+        }
+
+        for node in nodes.values():
+            await self.wavelink.initiate_node(**node)
+
+    def get_player(self, obj):
+        if isinstance(obj, commands.Context):
+            return self.wavelink.get_player(obj.guild.id, cls=Player, context=obj)
+        elif isinstance(obj, discord.Guild):
+            return self.wavelink.get_player(obj.id, cls=Player)
+
+    @commands.command(name="connect", aliases=["join"])
+    async def connect_command(self, ctx, *, channel: t.Optional[discord.VoiceChannel]):
+        player = self.get_player(ctx)
+        channel = await player.connect(ctx, channel)
+        await ctx.send(f"Connected to {channel.name}.")
+
+    @connect_command.error
+    async def connect_command_error(self, ctx, exc):
+        if isinstance(exc, AlreadyConnectedToChannel):
+            await ctx.send("Already connected to a voice channel.")
+        elif isinstance(exc, NoVoiceChannel):
+            await ctx.send("No suitable voice channel was provided.")
+
+    @commands.command(name="disconnect", aliases=["leave"])
+    async def disconnect_command(self, ctx):
+        player = self.get_player(ctx)
+        await player.teardown()
+        await ctx.send("Disconnect.")
+
+    @commands.command(name="play")
+    async def play_command(self, ctx, *, query: t.Optional[str]):
+        player = self.get_player(ctx)
+
+        if not player.is_connected:
+            await player.connect(ctx)
+
+        if query is None:
+            pass
+
+        else:
+            query = query.strip("<>")
+            if not re.match(URL_REGEX, query):
+                query = f"ytsearch:{query}"
+
+            # we can use this later if/when we need to just get the youtube video id
+            #testing = query[query.index("?v=") + 3:]
+            #print(query)
+            #print(testing)
+            
+            await player.add_tracks(ctx, await self.wavelink.get_tracks(query))
+
+
+def setup(bot):
+    bot.add_cog(Music(bot))
